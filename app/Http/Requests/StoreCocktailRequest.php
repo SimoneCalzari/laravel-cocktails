c058692533f9f4cfdf76c7269df80792bbbe3d6a
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2024-02-08 17:11:03 +0100

    feat: validation

---
+++ app/Http/Requests/StoreCocktailRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCocktailRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|string|max:20',
+            'alcolico' => 'required|boolean',
+            'ingredienti' => 'required|string|max:80',
+            'gradazione' => 'required|integer|max:255'
+        ];
+    }
+}
--- resources/views/cocktails/create.blade.php
+++ resources/views/cocktails/create.blade.php
@@ -2,26 +2,37 @@
 
 @section('main')
     <main>
+
         <div class="container">
+
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul class="list-unstyled">
+              @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+                     </ul>
+      </div>
+      @endif
             <form action="{{ route('cocktails.store') }}" method="POST">
                 @csrf
                 <div class="mb-3">
                     <label for="nome" class="form-label">Nome Cocktail</label>
-                    <input type="text" class="form-control" id="nome" aria-describedby="emailHelp" name="nome">
+                    <input type="text" class="form-control" id="nome" aria-describedby="emailHelp" name="nome" required value="{{ old('nome') }}">
                 </div>
                 <div class="mb-3">
                     <label for="ingredienti" class="form-label">Ingredienti</label>
                     <input type="text" class="form-control" id="ingredienti" aria-describedby="emailHelp"
-                        name="ingredienti">
+                        name="ingredienti" required value="{{ old('ingredienti') }}">
                 </div>
                 <div class="mb-3">
                     <label for="gradazione" class="form-label">Gradazione</label>
                     <input type="text" class="form-control" id="gradazione" aria-describedby="emailHelp"
-                        name="gradazione">
+                        name="gradazione" required value="{{ old('gradazione') }}">
                 </div>
 
                 <label for="alcolico" class="form-label">Alcolico</label>
-                <select class="form-select" aria-label="Default select example" name="alcolico" value="0">
+                <select class="form-select" aria-label="Default select example" name="alcolico">
 
                     <option value="1">si</option>
                     <option value="0">no</option>
